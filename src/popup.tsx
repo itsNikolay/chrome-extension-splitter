6a40772bac100996038f1b83760d71f89e7d6599
Nikolay Ponomarev <itsnikolay@gmail.com>
2021-07-03 02:55:47 +0300

    add react router

--- src/popup.tsx
+++ src/popup.tsx
@@ -1,10 +1,57 @@
 import React, { useState } from "react";
 import { render } from "react-dom";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  HashRouter
+} from "react-router-dom";
 
-const App = () => {
-  const [state, setState] = useState("CLICK ME");
+export default function App() {
+  return (
+    <HashRouter>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/users">Users</Link>
+            </li>
+          </ul>
+        </nav>
 
-  return <button onClick={() => setState("CLICKED")}>{state}</button>;
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/users">
+            <Users />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </HashRouter>
+  );
+}
+
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function About() {
+  return <h2>About</h2>;
+}
+
+function Users() {
+  return <h2>Users</h2>;
 }
 
 render(<App />, document.querySelector("#root"));
