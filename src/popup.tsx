39d0f5d4644542471b2fdd81a81d3e0dba6ba7aa
Nikolay Ponomarev <itsnikolay@gmail.com>
2021-07-04 01:03:58 +0300

    jshintrc

--- src/popup.tsx
+++ src/popup.tsx
@@ -1,10 +1,8 @@
-import React, { useState } from "react";
+import React from "react";
 import { render } from "react-dom";
 import {
-  BrowserRouter as Router,
   Switch,
   Route,
-  Link,
   HashRouter
 } from "react-router-dom";
 import Home from "./components/Home";
@@ -13,43 +11,13 @@ import './styles/style.css';
 export default function App() {
   return (
     <HashRouter>
-      <div>
-        {/* <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-            <li>
-              <Link to="/users">Users</Link>
-            </li>
-          </ul>
-        </nav> */}
-
-        <Switch>
-          {/* <Route path="/about">
-            <About />
-          </Route>
-          <Route path="/users">
-            <Users />
-          </Route> */}
-          <Route path="/">
-            <Home />
-          </Route>
-        </Switch>
-      </div>
+      <Switch>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
     </HashRouter>
   );
 }
-
-// function About() {
-//   return <h2>About</h2>;
-// }
-
-// function Users() {
-//   return <h2>Users</h2>;
-// }
 
 render(<App />, document.querySelector("#root"));
