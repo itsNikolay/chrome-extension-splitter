fc92221d442503664ba3835efb93cf4657ab5c91
Nikolay Ponomarev <itsnikolay@gmail.com>
2021-07-04 13:42:48 +0300

    refactoring

---
+++ src/components/FooterButtons.tsx
@@ -0,0 +1,15 @@
+
+import React from 'react'
+
+const FooterButtons = () => {
+  return (
+    <div className="grid grid-cols-4 mt-5">
+      <div className="border border-black">1</div>
+      <div className="border border-black">2</div>
+      <div className="border border-black">3</div>
+      <div className="border border-black">Donate</div>
+    </div>
+  )
+}
+
+export default FooterButtons
--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,33 +1,12 @@
 import React from 'react'
-
-const res = [
-  [12, null],
-  [6, 6],
-  [3, 9],
-  [9, 3],
-]
+import FooterButtons from './FooterButtons'
+import Grid from './Grid'
 
 const Home = () => {
   return (
-    <div className="container mx-auto w-96 mt-10">
-      <div className="grid grid-row-3 gap-5">
-        {res.map((row, index) => {
-          return (
-            <button key={index} className="hover:shadow-lg transition-all">
-              <div className="h-12 grid grid-cols-12 gap-2">
-                {row.filter((num) => num !== null).map((num, index2) => {
-                  return (
-                    <div
-                      className={`border border-blue-500 col-span-${num}`}
-                      key={index2}
-                    ></div>
-                  )
-                })}
-              </div>
-            </button>
-          )
-        })}
-      </div>
+    <div className="container mx-auto w-96 p-5">
+      <Grid />
+      <FooterButtons />
     </div>
   )
 }
