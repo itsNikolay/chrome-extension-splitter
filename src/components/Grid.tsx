5f190c543ab2e9bdb7b99c060fea1cabd1a26dcf
Nikolay Ponomarev <itsnikolay@gmail.com>
2021-07-04 13:54:56 +0300

    obj entries

--- src/components/Grid.tsx
+++ src/components/Grid.tsx
@@ -1,25 +1,25 @@
 import React from 'react'
 
-const res = [
-  [12, null],
-  [6, 6],
-  [3, 9],
-  [9, 3],
-  [6, 6, 6, 6],
-]
+const res = {
+  oneTwelve: [12, null],
+  twoSix: [6, 6],
+  twoThree: [3, 9],
+  twoNine: [9, 3],
+  fourSix: [6, 6, 6, 6],
+}
 
 const Grid = () => {
   return (
     <div className="grid grid-row-3 gap-5">
-      {res.map((row, index) => {
+      {Object.entries(res).map(([key, row]) => {
         return (
-          <button key={index} className="hover:shadow-lg transition-all">
+          <button key={key} className="hover:shadow-lg transition-all">
             <div className="h-12 grid grid-cols-12 gap-2">
-              {row.filter((num) => num !== null).map((num, index2) => {
+              {row.filter((num) => num !== null).map((num, index) => {
                 return (
                   <div
                     className={`border border-blue-500 col-span-${num}`}
-                    key={index2}
+                    key={index}
                   ></div>
                 )
               })}
