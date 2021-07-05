592fd1da7e4aaa9231a804b6651960391164c015
Nikolay Ponomarev <itsnikolay@gmail.com>
2021-07-05 03:33:25 +0300

    refactoring

--- src/lib/updateInfo.ts
+++ src/lib/updateInfo.ts
@@ -1,36 +1,46 @@
-import { getAppWindow } from "./appWindow";
 import { Columns, getRowsAmount, MAX_COLUMNS } from "./columnsInfo";
 
+type AppScreen = Partial<typeof window.screen>
 type Info = chrome.windows.UpdateInfo
 
 interface Props {
   columns: Columns
-  screen: Partial<typeof window.screen>
+  screen: AppScreen
+}
+
+const getWindowInfo = (screen: AppScreen) => {
+  const screenWidth = screen.width || 1024
+  const screenHeight = screen.height || 768
+
+  return (maxColumns: number) => (col: number) => (totalWidth: number) => (totalRows: number) => {
+    const rowsAmount = Math.floor(totalWidth / screenWidth)
+    const width = (screenWidth / maxColumns) * col
+    const left = totalWidth % screenWidth
+    const height = screenHeight / totalRows
+    const top = rowsAmount * height
+    const focused = true
+
+    return ({
+      width,
+      top,
+      height,
+      left,
+      focused
+    })
+  }
 }
 
 const updateData = ({ columns, screen }: Props): Info[] => {
-  const initAppWindow = getAppWindow(screen)
+  const initWindowInfo = getWindowInfo(screen)
   const totalRows = getRowsAmount(columns)
 
   return columns.reduce((acc, col) => {
     const totalWidth = acc.map((a) => a.width || 0).reduce((a, b) => a + b, 0)
-    const appWindow = initAppWindow(totalWidth)
-
-    const width = appWindow.getWidth(MAX_COLUMNS, col)
-    const height = appWindow.getHeight(totalRows)
-    const left = appWindow.getLeft()
-    const top = appWindow.getTop(height)
-    const focused = true
+    const windowInfo = initWindowInfo(MAX_COLUMNS)(col)(totalWidth)(totalRows)
 
     return [
       ...acc,
-      {
-        width,
-        height,
-        top,
-        left,
-        focused
-      }
+      windowInfo
     ]
   }, [] as Info[])
 
