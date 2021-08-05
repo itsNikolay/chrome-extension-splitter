a0ae00e50e5902ae71bd135e81c03d0242ca89b5
Nikolay Ponomarev <itsnikolay@gmail.com>
2021-08-05 23:00:17 +0300

    footer buttons

--- src/components/FooterButtons.tsx
+++ src/components/FooterButtons.tsx
@@ -1,18 +1,18 @@
-
 import React from 'react'
 import DonateIcon from './icons/DonateIcon'
 
 const FooterButtons = () => {
   return (
     <div className="grid grid-cols-auto mt-5 justify-end">
-      <div className="">
+      <div className="text-center">
         <a
-          href="http://nponomarev.com"
+          href="https://nponomarev.com"
           target="_blank"
           rel="noopener noreferrer"
-          className="inline-block fill-current hover:text-blue-500 w-20"
+          className="inline-block fill-current hover:text-blue-500 w-12 transition-colors duration-200"
         >
           <DonateIcon />
+          <div>Donate</div>
         </a>
       </div>
     </div>
