c1bab350ce37700fc736a554216514b1ee044468
Nikolay Ponomarev <itsnikolay@gmail.com>
2021-08-08 16:08:23 +0300

    About Dialog

--- src/components/FooterButtons.tsx
+++ src/components/FooterButtons.tsx
@@ -1,17 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
+import AboutDialog from './AboutDialog'
 
 const FooterButtons = () => {
+  let [isOpen, setIsOpen] = useState(true)
+
   return (
     <div className="grid grid-cols-auto grid-flow-col mt-5 gap-6">
       <div className="text-center">
-        <a
-          href="https://forms.gle/AVdW2FD1hHC8YMst5"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="inline-block underline fill-current hover:text-blue-500 w-12 transition-colors duration-200"
+        <button
+          onClick={() => setIsOpen(true)}
+          className="inline-block fill-current hover:text-blue-500 w-12 transition-colors duration-200"
         >
           About
-        </a>
+        </button>
+        {isOpen && <AboutDialog isOpen={isOpen} setIsOpen={setIsOpen} />}
       </div>
       <div className="text-center">
         <a
