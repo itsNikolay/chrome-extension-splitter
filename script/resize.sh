0e5085fbf4b6d07cd0e0221699a8bdd68b42ad4f
Nikolay Ponomarev <itsnikolay@gmail.com>
2021-08-08 15:43:20 +0300

    buttons and icons

---
+++ script/resize.sh
@@ -0,0 +1,7 @@
+#! /bin/sh
+
+convert icon_orig.png -resize 16x16 icon16.png && \
+convert icon_orig.png -resize 32x32 icon32.png && \
+convert icon_orig.png -resize 48x48 icon48.png && \
+convert icon_orig.png -resize 128x128 icon128.png && \
+cp icon16.png icon32.png icon48.png icon128.png src/icons/
--- src/components/icons/DonateIcon.tsx
+++ src/components/icons/DonateIcon.tsx
@@ -3,9 +3,9 @@ import React from 'react'
 const DonateIcon = () => {
   return (
     <svg
-      height="512"
+      height="48"
+      width="48"
       viewBox="0 0 64 64"
-      width="512"
       xmlns="http://www.w3.org/2000/svg"
       className="w-full h-full"
     >
